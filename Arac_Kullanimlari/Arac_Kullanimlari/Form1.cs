e61bef6e18394ee6e3345f20ec5c906cc1c341f4
barbarossaalagoz-spec <barbarossa.alagoz@gmail.com>
2025-10-15 20:20:52 +0300

    Add files via upload

---
+++ Arac_Kullanimlari/Arac_Kullanimlari/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Arac_Kullanimlari
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Merhaba Dünya!");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label2.Text = "Baran";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            label6.Text = "Barbaros Emre";
+            label7.Text = "Alagöz";
+            label8.Text = "Gayrimenkul Danışmanı";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            // textBox1.Text = "Yazılım Mühendisliği";
+            // nereye yazılacaksa orası solda, nereden yazılacaksa orası sağda
+            label9.Text = textBox1.Text;
+        }
+    }
+}
---
+++ Arac_Kullanimlari_2/Arac_Kullanimlari_2/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Arac_Kullanimlari_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            comboBox1.Items.Add("Ankara");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            comboBox1.Items.Add(textBox1.Text);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add("Kasiyer");
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add(textBox2.Text);
+        }
+    }
+}
