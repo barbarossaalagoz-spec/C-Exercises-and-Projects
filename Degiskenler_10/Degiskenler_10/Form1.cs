0bc5f2a77c7554039d51e6247a659030264d1db3
barbarossaalagoz-spec <barbarossa.alagoz@gmail.com>
2025-10-17 09:49:09 +0300

    Add files via upload

---
+++ Degiskenler_10/Degiskenler_10/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Degiskenler_10
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //double sayi;
+            //sayi = 4.25;
+            //label1.Text = sayi.ToString();
+
+            double s1,s2,s3,ortalama;
+            s1 = 60;
+            s2 = 70;
+            s3 = 79;
+            ortalama = (s1 + s2 + s3) / 3;
+
+            label1.Text = "Ortalama: " + ortalama.ToString("0.00");
+
+            // double veri tipi ondalıklı sayılar için kullanılır.
+            // Ondalıklı sayılar virgül veya nokta ile yazılabilir.
+            // Ancak C# dilinde ondalık ayıracı olarak nokta kullanılır.
+            // Virgül kullanılırsa derleme hatası oluşur.
+            // Örnek: double sayi = 4,25; // Hatalı
+            // Doğru kullanım: double sayi = 4.25;
+            // double veri tipi 8 byte (64 bit) yer kaplar.
+            // double veri tipi yaklaşık olarak ±5.0 × 10^−324 ile ±1.7 × 10^308 arasındaki değerleri alabilir.
+            // double veri tipi ondalıklı sayılar için yüksek hassasiyet gerektiren durumlarda tercih edilir.
+            // Ancak daha fazla bellek kullanır ve işlemler float veri tipine göre daha yavaştır.
+            // Eğer daha az hassasiyet yeterliyse float veri tipi tercih edilebilir.
+            // float veri tipi 4 byte (32 bit) yer kaplar ve yaklaşık olarak ±1.5 × 10^−45 ile ±3.4 × 10^38 arasındaki değerleri alabilir.
+            // Örnek: float sayi = 4.25f; // 'f' eki float olduğunu belirtir.
+            // double veri tipi genellikle finansal hesaplamalar, bilimsel veriler ve mühendislik uygulamalarında kullanılır.
+            // double veri tipi ile çalışırken yuvarlama hatalarına dikkat edilmelidir.
+            // Özellikle karşılaştırma işlemlerinde doğrudan eşitlik kontrolü yerine belirli bir tolerans aralığında kontrol yapmak daha güvenlidir.
+            // to.String("0.00") ifadesi, sayının sadece iki ondalık basamağını göstermeyi sağlar.
+        }
+    }
+}
---
+++ Degiskenler_10/Degiskenler_10/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Degiskenler_10")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Degiskenler_10")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("aa81cd47-bd62-4e5b-9474-3a3563f4198b")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
