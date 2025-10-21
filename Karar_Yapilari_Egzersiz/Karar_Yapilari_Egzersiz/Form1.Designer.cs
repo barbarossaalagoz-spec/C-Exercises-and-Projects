6874ad6b354d407921243896d3fba9b9f8671b22
Barbaros Emre ALAGÖZ <barbarossa.alagoz@gmail.com>
2025-10-21 10:58:27 +0300

    Add files via upload

---
+++ Karar_Yapilari_Egzersiz/Karar_Yapilari_Egzersiz/Form1.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace Karar_Yapilari_Egzersiz
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TxtSinav1 = new System.Windows.Forms.TextBox();
+            this.TxtSinav2 = new System.Windows.Forms.TextBox();
+            this.TxtOrt = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.TxtAd = new System.Windows.Forms.TextBox();
+            this.TxtSoyad = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // TxtSinav1
+            // 
+            this.TxtSinav1.Location = new System.Drawing.Point(164, 143);
+            this.TxtSinav1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.TxtSinav1.Name = "TxtSinav1";
+            this.TxtSinav1.Size = new System.Drawing.Size(148, 26);
+            this.TxtSinav1.TabIndex = 0;
+            // 
+            // TxtSinav2
+            // 
+            this.TxtSinav2.Location = new System.Drawing.Point(163, 179);
+            this.TxtSinav2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.TxtSinav2.Name = "TxtSinav2";
+            this.TxtSinav2.Size = new System.Drawing.Size(148, 26);
+            this.TxtSinav2.TabIndex = 1;
+            // 
+            // TxtOrt
+            // 
+            this.TxtOrt.Enabled = false;
+            this.TxtOrt.Location = new System.Drawing.Point(164, 215);
+            this.TxtOrt.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.TxtOrt.Name = "TxtOrt";
+            this.TxtOrt.Size = new System.Drawing.Size(148, 26);
+            this.TxtOrt.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.button1.Location = new System.Drawing.Point(175, 283);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(150, 35);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "HESAPLA";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(43, 146);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(107, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "1. Sınav Notu:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(43, 182);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(107, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "2. Sınav Notu:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(72, 218);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(78, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Ortalama:";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(370, 77);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(675, 164);
+            this.listBox1.TabIndex = 7;
+            // 
+            // TxtAd
+            // 
+            this.TxtAd.Location = new System.Drawing.Point(163, 74);
+            this.TxtAd.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.TxtAd.Name = "TxtAd";
+            this.TxtAd.Size = new System.Drawing.Size(148, 26);
+            this.TxtAd.TabIndex = 8;
+            // 
+            // TxtSoyad
+            // 
+            this.TxtSoyad.Location = new System.Drawing.Point(164, 107);
+            this.TxtSoyad.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.TxtSoyad.Name = "TxtSoyad";
+            this.TxtSoyad.Size = new System.Drawing.Size(148, 26);
+            this.TxtSoyad.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(117, 77);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(33, 20);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Ad:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(92, 110);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(58, 20);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Soyad:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(1094, 346);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.TxtSoyad);
+            this.Controls.Add(this.TxtAd);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.TxtOrt);
+            this.Controls.Add(this.TxtSinav2);
+            this.Controls.Add(this.TxtSinav1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Sınav Not Ortalama Programı";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TxtSinav1;
+        private System.Windows.Forms.TextBox TxtSinav2;
+        private System.Windows.Forms.TextBox TxtOrt;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox TxtAd;
+        private System.Windows.Forms.TextBox TxtSoyad;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Karar_Yapilari_Egzersiz/Karar_Yapilari_Egzersiz/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Karar_Yapilari_Egzersiz
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string ad, soyad;
+            ad = TxtAd.Text;
+            soyad = TxtSoyad.Text;
+
+            int sinav1,sinav2, ortalama;
+            sinav1 = Convert.ToInt16(TxtSinav1.Text);
+            sinav2 = Convert.ToInt16(TxtSinav2.Text);
+            ortalama = (sinav1 + sinav2) / 2;
+            TxtOrt.Text = ortalama.ToString();
+
+
+            listBox1.Items.Add("Ad: " + ad + " " + " Soyad: " + soyad + " 1. Sınav Notu: " + sinav1 + " " + " 2.Sınav Notu: " + sinav2 + " " + " Ortalama: " + ortalama);
+
+            if (ortalama >= 50)
+            {
+                MessageBox.Show("Tebrikler, dersten geçtiniz. Notunuz: " + ortalama);
+            }
+            else
+            {
+                MessageBox.Show("Maalesef dersten kaldınız. Notunuz: " + ortalama);
+            }
+
+
+        }
+    }
+}
