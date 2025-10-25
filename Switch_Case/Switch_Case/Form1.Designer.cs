765e9b901d39fb1f18d6781bed76c0a149cf59f3
Barbaros Emre ALAGÖZ <barbarossa.alagoz@gmail.com>
2025-10-25 13:16:05 +0300

    Add files via upload

---
+++ Switch_Case/Switch_Case/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace Switch_Case
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Sayi1 = new System.Windows.Forms.TextBox();
+            this.Sayi2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(130, 172);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(85, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "İŞLEMİ YAP";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(88, 76);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Sayı1:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(153, 239);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "label2";
+            // 
+            // Sayi1
+            // 
+            this.Sayi1.Location = new System.Drawing.Point(130, 73);
+            this.Sayi1.Name = "Sayi1";
+            this.Sayi1.Size = new System.Drawing.Size(121, 20);
+            this.Sayi1.TabIndex = 3;
+            // 
+            // Sayi2
+            // 
+            this.Sayi2.Location = new System.Drawing.Point(130, 99);
+            this.Sayi2.Name = "Sayi2";
+            this.Sayi2.Size = new System.Drawing.Size(121, 20);
+            this.Sayi2.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(88, 102);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(36, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Sayı2:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(47, 128);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(77, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Aritmetik İşlem:";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Topla",
+            "Çıkar",
+            "Çarp",
+            "Böl"});
+            this.comboBox1.Location = new System.Drawing.Point(130, 125);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(358, 314);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.Sayi2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.Sayi1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox Sayi1;
+        private System.Windows.Forms.TextBox Sayi2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ Switch_Case/Switch_Case/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Switch_Case
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //int ay = Convert.ToInt32(textBox1.Text);
+
+            //switch (ay)
+            //{
+            //    case 1: label2.Text = "Ocak"; break;
+            //    case 2: label2.Text = "Şubat"; break;
+            //    case 3: label2.Text = "Mart"; break;
+            //    case 4: label2.Text = "Nisan"; break;
+            //    case 5: label2.Text = "Mayıs"; break;
+            //    case 6: label2.Text = "Haziran"; break;
+            //    case 7: label2.Text = "Temmuz"; break;
+            //    case 8: label2.Text = "Ağustos"; break;
+            //    case 9: label2.Text = "Eylül"; break;
+            //    case 10: label2.Text = "Ekim"; break;
+            //    case 11: label2.Text = "Kasım"; break;
+            //    case 12: label2.Text = "Aralık"; break;
+            //    default: label2.Text = "Hatalı Ay"; break;
+            //}
+
+            // bu aylarla ilgili int kapsayan bir şeydi; şimdi mevsimlerle ilgili kod yazıcaz
+
+            //string mevsim = textBox1.Text;
+
+            //switch (mevsim)
+            //{
+            //    case "yaz": label2.Text = "haziran temmuz ağustos";
+            //        break;
+            //    case "sonbahar": label2.Text = "eylül ekim kasım";
+            //        break;
+            //    case "kış": label2.Text = "aralık ocak şubar";
+            //        break;
+            //    case "ilkbahar": label2.Text = "mart nisan mayıs";
+            //        break;
+            //    default: label2.Text = "Hatalı Mevsim!";
+            //        break;
+            //}
+
+            // şimdi ise iki farklı sayı verilen rakama göre aritmetik işlem sonucunu verecek iki farklı textboc üzerinden
+
+            int sayi1, sayi2, toplama, cikarma, carpma, bolme;
+            sayi1 = Convert.ToInt16(Sayi1.Text);
+            sayi2 = Convert.ToInt16(Sayi2.Text);
+
+            toplama = sayi1 + sayi2;
+            cikarma = sayi1 - sayi2;
+            carpma = sayi1 * sayi2;
+            bolme = sayi1 / sayi2;
+
+            string girilen_deger = comboBox1.Text; // ComboBox'taki seçilen veya yazılan değer
+
+            switch (girilen_deger)
+            {
+                case "Topla":
+                    label2.Text = toplama.ToString();
+                    break;
+
+                case "Çıkar":
+                    label2.Text = cikarma.ToString();
+                    break;
+
+                case "Çarp":
+                    label2.Text = carpma.ToString();
+                    break;
+
+                case "Böl":
+                    label2.Text = bolme.ToString();
+                    break;
+
+                default:
+                    label2.Text = "Geçersiz işlem!";
+                    break;
+            }
+        }
+    }
+}
