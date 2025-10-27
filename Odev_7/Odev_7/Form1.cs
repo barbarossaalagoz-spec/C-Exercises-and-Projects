dabe1dc64f72c3b03ef4a66b42061388c33af5ed
Barbaros Emre ALAGÖZ <barbarossa.alagoz@gmail.com>
2025-10-27 03:43:32 +0300

    Add files via upload

---
+++ Odev_7/Odev_7/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Odev_7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // girilen sayının faktöriyelini hesaplama
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int sayi = Convert.ToInt16(textBox1.Text);
+            int faktoriyel = 1;
+
+            for (int i = 1; i <= sayi; i++)
+            {
+                faktoriyel = faktoriyel * i;
+                listBox1.Items.Add(faktoriyel);
+            }
+        }
+    }
+}
