97086078e9ed5cab47ebb4b6bda3dd17d3fbe1a4
Barbaros Emre ALAGÖZ <barbarossa.alagoz@gmail.com>
2025-10-27 03:44:36 +0300

    Add files via upload

---
+++ Donguler_3/Donguler_3/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Donguler_3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int toplam = 0;
+            for (int i = 2; i <= 10; i += 2)
+            {
+                toplam = toplam + i;
+            }
+
+            label2.Text = toplam.ToString();
+        }
+    }
+}
